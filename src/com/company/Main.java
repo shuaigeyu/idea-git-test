bc61bf047f83df03dcc58b1ee9261c1e0a96b895
shuaigeyu <42038116+shuaigeyu@users.noreply.github.com>
2021-07-30 11:26:03 +0800

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("xiecheng");
-	    System.out.print("xiecheng1");
-        System.out.print("xiecheng2");
-        System.out.print("xiecheng");
-        System.out.print("xiecheng1");
-        System.out.print("xiecheng2");
-        System.out.print("xiecheng");
-        System.out.print("xiecheng1");
-        System.out.print("xiecheng2");
+        
 
     }
 }
