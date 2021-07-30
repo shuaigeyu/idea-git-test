91846286488f83b199af89bb618800dbb30b974e
my_fan <my_fan@trip.com>
2021-07-30 11:15:10 +0800

    第一次提交到本地仓库

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,8 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.print("xiecheng");
-	System.out.print("xiecheng1");
-	
+	    System.out.print("xiecheng1");
+        System.out.print("xiecheng2");
+        System.out.print("xiecheng2");
     }
 }
